46d3d73cf3799b5bf659df16ecf61fcacc2a74ea
ganquanfu <ganquanfu@csin.net.cn>
2022-09-19 17:13:46 +0800

    优化视频弹窗视频加载不出的问题

--- src/components/homeYiluvideo.vue
+++ src/components/homeYiluvideo.vue
@@ -63,25 +63,25 @@ export default {
                         id: 'homeYiluvideo', // 视频容器ID
                         accessToken: res.data,
                         url: 'ezopen://open.ys7.com/' + (serialNumber ? serialNumber : 'J63364396') + '/1.live',
-                        template: 'simple', // simple - 极简版;standard-标准版;security - 安防版(预览回放);voice-语音版；
+                        // template: 'simple', // simple - 极简版;standard-标准版;security - 安防版(预览回放);voice-语音版；
                         // 视频上方头部控件
-                        header: ['capturePicture', 'save', 'zoom'], // 如果templete参数不为simple,该字段将被覆盖
-                        plugin: ['talk', 'broadcast'], // 加载插件，talk-对讲
+                        // header: ['capturePicture', 'save', 'zoom'], // 如果templete参数不为simple,该字段将被覆盖
+                        // plugin: ['talk', 'broadcast'], // 加载插件，talk-对讲
                         // 视频下方底部控件
-                        footer: ['talk', 'broadcast', 'hd', 'fullScreen'], // 如果template参数不为simple,该字段将被覆盖
-                        audio: 1, // 是否默认开启声音 0 - 关闭 1 - 开启
-                        openSoundCallBack: (data) => console.log('开启声音回调', data),
-                        closeSoundCallBack: (data) => console.log('关闭声音回调', data),
-                        startSaveCallBack: (data) => console.log('开始录像回调', data),
-                        stopSaveCallBack: (data) => console.log('录像回调', data),
-                        capturePictureCallBack: (data) => console.log('截图成功回调', data),
-                        fullScreenCallBack: (data) => {
-                            console.log(123);
-                            this.$emit('changeFull');
-                        },
-                        getOSDTimeCallBack: (data) => console.log('获取OSDTime回调', data),
-                        height: 260,
-                        width: 550
+                        // footer: ['talk', 'broadcast', 'hd', 'fullScreen'], // 如果template参数不为simple,该字段将被覆盖
+                        // audio: 1, // 是否默认开启声音 0 - 关闭 1 - 开启
+                        // openSoundCallBack: (data) => console.log('开启声音回调', data),
+                        // closeSoundCallBack: (data) => console.log('关闭声音回调', data),
+                        // startSaveCallBack: (data) => console.log('开始录像回调', data),
+                        // stopSaveCallBack: (data) => console.log('录像回调', data),
+                        // capturePictureCallBack: (data) => console.log('截图成功回调', data),
+                        // fullScreenCallBack: (data) => {
+                        //     console.log(123);
+                        //     this.$emit('changeFull');
+                        // },
+                        // getOSDTimeCallBack: (data) => console.log('获取OSDTime回调', data),
+                        height: '260',
+                        width: '550'
                     });
                 }
             });
